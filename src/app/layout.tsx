83937b3e2e5451d16f459a071b21879e4dc71157
Yash <78804783+yashd-dev@users.noreply.github.com>
2025-09-01 14:34:16 +0530

    feat: revamped entire ui and added legal name for payment gateways

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PixelBlast from "@/components/PixelBlast/PixelBlast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
